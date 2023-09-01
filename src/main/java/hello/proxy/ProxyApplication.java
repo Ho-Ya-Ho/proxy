63a1f36f65b8c4133c8414ebf7bbaba476d58577
Ho-Ya-Ho <rkdgh1056@gmail.com>
2023-09-01 10:23:05 +0900

    Concerete 프록시

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -2,6 +2,7 @@ package hello.proxy;
 
 import hello.proxy.config.AppV1Config;
 import hello.proxy.config.AppV2Config;
+import hello.proxy.config.v1_proxy.ConcreteProxyConfig;
 import hello.proxy.config.v1_proxy.InterfaceProxyConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
@@ -11,7 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
-@Import(InterfaceProxyConfig.class) //Config를 여러개 만들어서 원하는 것들만 Spring Container에 올리기위해서
+@Import(ConcreteProxyConfig.class) //Config를 여러개 만들어서 원하는 것들만 Spring Container에 올리기위해서
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의 -해당 경로 아래의 파일들만 ComponentScan의 대상이 된다.-
 public class ProxyApplication {
 
